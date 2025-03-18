d5b90156d4f219a06094d233e98e7c99ffa8b68c
mazzatenta2140428 <mazzatenta.2140428@studenti.uniroma1.it>
2025-03-18 22:17:43 +0100

    small update on tsne and kmeans

--- data/pca.py
+++ data/pca.py
@@ -9,7 +9,7 @@ from sklearn.metrics import silhouette_score, silhouette_samples
 
 # Define the columns to drop
 columns_to_drop = [
-    "potential", "version", "club_kit_number", "club_rating", "club_league_id", "club_id",
+    "version", "club_kit_number", "club_rating", "club_league_id", "club_id",
      "club_jersey_number", "club_loaned_from", "club_joined", "club_contract_valid_until",
     "nation_team_id", "nation_jersey_number", "work_rate", "body_type", "release_clause",
      "country_position", "country_kit_number", "country_rating", "country_league_name", "country_league_id" 
@@ -22,9 +22,10 @@ df = pd.read_csv(file_path)
 ## Filter only FIFA 23 players
 
 df = df[df["overall_rating"] >= 65]
-
+df2 = df.replace("", np.nan)  # Convert "" to NaN
+df3 = df2.dropna(subset=["gk_diving","gk_handling","gk_kicking","gk_positioning","gk_reflexes"])  # Now drop NaNs
 # Drop the unwanted columns
-df_cleaned = df.drop(columns=columns_to_drop, errors='ignore')
+df_cleaned = df3.drop(columns=columns_to_drop, errors='ignore')
 
 # Drop rows where 'value_eur' is null
 df_cleaned = df_cleaned.dropna(subset=["value"])
@@ -51,6 +52,8 @@ column_means_original = np.round(df_cleaned[numeric_columns_original].min())
 numeric_columns_pca = df_pca_kmeans.select_dtypes(include=[np.number]).columns
 column_means_pca = np.round(df_pca_kmeans[numeric_columns_pca].min())
 
+
+
 # Fill the NaN values with the respective column means for only numeric columns
 df_cleaned_pca = df_pca_kmeans.fillna(column_means_pca)
 df_output = df_cleaned.fillna(column_means_original)
@@ -60,11 +63,15 @@ print(df_cleaned["club_league_name"].unique())
 #print(df_cleaned_gk2.info())
 # Columns to use for t-SNE
 columns_for_tsne = [
-    "overall_rating", "acceleration", "sprint_speed", "dribbling", "shot_power", "long_shots", "short_passing",  "long_passing", "defensive_awareness", "strength", "stamina", "height_cm", "weight_kg"
+    "crossing","finishing","heading_accuracy","short_passing","volleys","dribbling","curve",
+    "fk_accuracy","long_passing","ball_control","acceleration","sprint_speed","agility","reactions",
+    "balance","shot_power","jumping","stamina","strength","long_shots","aggression","interceptions","positioning",
+    "vision","penalties","composure","defensive_awareness","standing_tackle","sliding_tackle"
 ]
 
 # Select only the columns for t-SNE
 df_for_tsne = df_cleaned_pca[columns_for_tsne]
+
 
 ## One-Hot Encoding of categorical features
 #categorical_features = ["club_position"]
@@ -76,14 +83,14 @@ df_scaled = scaler.fit_transform(df_for_tsne)  # Drop NaN to avoid errors
 
 # First t-SNE Reduction: Perform t-SNE for Dimensionality Reduction to 2 Components (intermediate step)
 from sklearn.manifold import TSNE
-tsne_20 = TSNE(n_components=2, random_state=42)
+tsne_20 = TSNE(n_components=2, random_state=23)
 final_components = tsne_20.fit_transform(df_scaled)
 
 # Convert the output into a proper DataFrame
 df_tsne = pd.DataFrame(data=final_components, columns=["Dim1", "Dim2"])
 
 # Find Best K Using Silhouette Score
-k_values = range(2, 20)
+k_values = range(2, 10)
 silhouette_scores = []
 
 for k in k_values:
@@ -108,7 +115,7 @@ best_k = k_values[np.argmax(silhouette_scores)]
 print(f"\nBest Number of Clusters: {best_k}")
 
 # Apply K-Means with Best K
-kmeans = KMeans(n_clusters=best_k, random_state=None, n_init=10)
+kmeans = KMeans(n_clusters=best_k, random_state=23, n_init=10)
 df_tsne["Cluster"] = kmeans.fit_predict(df_tsne[["Dim1", "Dim2"]])
 
 # Copy the Tsne_Dim1, Tsne_Dim2, and Cluster columns from df_tsne into df_cleaned_gk
